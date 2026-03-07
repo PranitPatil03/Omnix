81844e3aeec64ca3e1fb051aff2ab39c785929c1
pranitpatil03 <patilpranit3112@gmail.com>
2026-03-08 02:48:08 +0530

    feat: Revamp authentication views with new layout and improved UI components for sign-in and sign-up

--- apps/web/modules/auth/ui/layouts/auth-layout.tsx
+++ apps/web/modules/auth/ui/layouts/auth-layout.tsx
@@ -1,6 +1,6 @@
 export const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return ( 
-    <div className="min-h-screen min-w-screen h-full flex flex-col items-center justify-center">
+  return (
+    <div className="min-h-screen min-w-screen h-full flex items-center justify-center px-6 py-12">
       {children}
     </div>
   );
